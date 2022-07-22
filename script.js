e7479fe3387a0d8b836c06f636e46a603c9130fc
Samson Cheung <samsonc89@yahoo.com>
2022-07-22 11:22:23 -0700

    Add functionality to 'black' button

--- script.js
+++ script.js
@@ -11,10 +11,16 @@ after divs are made select ALL of them and add class list with loop
 css grid width/height calculation is then changed to a
 
 */
+//declarations
 const gridContainer = document.querySelector(".grid-container");
 const gridRules = document.styleSheets[0].cssRules[4];
 const userColor = document.querySelector("#color");
 const gridText = document.querySelector("#grid-size");
+let randomColor;
+const randomColorGenerator = function () {
+  return (randomColor =
+    "#" + Math.floor(Math.random() * 16777215).toString(16));
+};
 
 userColor.onchange = (e) => {
   console.log(e.target.value);
@@ -22,17 +28,19 @@ userColor.onchange = (e) => {
 
 let rows = 10;
 
-let currentColor = "rgb(0, 0, 0)";
+let currentColor = "#000000";
 
+//functions
 const gridSize = function () {
   gridRules.style.cssText = `box-sizing: border-box;
     border: 1px solid black; width: calc(100% / ${rows}); height: calc(100% / ${rows});`;
 };
 
-const addSquares = function () {
+const addColor = function (color = "#000000") {
   const squares = document.querySelectorAll(".grid");
   squares.forEach((square) =>
     square.addEventListener("mouseenter", () => {
+      currentColor = color;
       square.style.backgroundColor = `${currentColor}`;
     })
   );
@@ -48,10 +56,22 @@ const createDiv = function () {
     div.classList = "grid";
     gridContainer.appendChild(div);
   }
-  addSquares();
+  addColor();
 };
 
 createDiv();
+
+// const state = function (color) {
+//     //changes color based on input
+//     currentColor = color;
+//     const squares = document.querySelectorAll(".grid");
+//   squares.forEach((square) =>
+//     square.addEventListener("mouseenter", () => {
+//       square.style.backgroundColor = `${currentColor}`;
+//     })
+//   );
+// };
+
 const clearGrid = function () {
   const squares = document.querySelectorAll(".grid");
   squares.forEach((square) => {
@@ -73,18 +93,38 @@ const resize = function () {
   gridText.textContent = `${rows} x ${rows}`;
 };
 
-//functionality for reset button
+const rainbowGenerator = function () {};
+
+//functionality for buttons
+//reset button
 document.querySelector("#reset").addEventListener("click", clearGrid);
 
-//functionality for resize button
+//resize button
 document.querySelector("#resize").addEventListener("click", resize);
+
+//rainbow
+document.querySelector("#rainbow-picker").addEventListener("click", () => {
+  const squares = document.querySelectorAll(".grid");
+  squares.forEach((square) =>
+    square.addEventListener("mouseenter", () => {
+      randomColorGenerator();
+      currentColor = randomColor;
+      square.style.backgroundColor = `${currentColor}`;
+      console.log(randomColor);
+    })
+  );
+});
+
+document.querySelector("#black").addEventListener("click", () => {
+  addColor();
+});
 
 //functionality for color picker
 
 /*
 
 each color button changes a state
-the state modifies which color
+the state modifies which color is current
 
 user
 
